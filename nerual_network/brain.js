979745221855403e09568a7381e2d7294efb657f
demchenko.daniil.HP <dan9m@yandex.com>
2016-09-22 19:15:27 +0300

    normalize data

--- nerual_network/brain.js
+++ nerual_network/brain.js
@@ -1,26 +1,75 @@
 "use strict";
 
 var xlsx = require('node-xlsx');
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
+var brain = require('brain');
 
+var net = new brain.NeuralNetwork();
 var objAuto = xlsx.parse('testdb.csv.xlsx'); // parses a file
 
-function processingData(engine, timeAndOther)
-{
-    var $ = cheerio.load(engine);
-    let textHP = $('table > tr:nth-last-child(3) > td').text();
+var dataAuto = [];
+const lengthData = 10;
 
-    textHP = textHP.split(')',2)[1];
-
-    console.log(textHP);
-}
-
-for (var i=1; i<10; i++)
+for (var i=1; i<=lengthData; i++)
 {
     let engine = objAuto[0].data[i][7];
     let timeAndOther = objAuto[0].data[i][10];
 
-    processingData(engine, timeAndOther)
+    processingData(engine, timeAndOther);
+
+    if (i==lengthData)
+    {
+        normalize(dataAuto, 'input', 'time');
+        normalize(dataAuto, 'output', 'hp');
+       console.log(dataAuto);
+        brainTraning(dataAuto);
+    }
 
 }
-// var obj = xlsx.parse(fs.readFileSync(__dirname + '/myFile.xlsx')); // parses a buffer
+
+function processingData(engine, timeAndOther)
+{
+    var $ = cheerio.load(engine);
+    let textHP = $('table > tr:nth-last-child(3) > td:nth-child(2)').text();//magic selector
+    let intHP = parseInt(textHP);
+    //-----
+    $ = cheerio.load(timeAndOther);
+    let textTime = $('table > tr:nth-last-child(3) > td:nth-child(2)').text();//magic selector
+    let floatTime = parseFloat(textTime);
+    //-----
+    dataAuto.push(
+        {
+            input:{time:floatTime}, output:{hp:intHP}
+        }
+    );
+    //console.log(intHP+' - '+floatTime);
+}
+
+function normalize(arr, io, fieldName)
+{
+    let max = 0;
+    arr.forEach(function (item)
+    {
+        if (item[io][fieldName]>max)
+        {
+            max = item[io][fieldName];
+        }
+
+    });
+
+    arr.map(function (item)
+    {
+        item[io][fieldName] /= max;
+    });
+
+    return arr;
+
+}
+
+function brainTraning(data)
+{
+    net.train(data);
+
+    var output = net.run({time:5}); 
+    console.log(output);
+}
